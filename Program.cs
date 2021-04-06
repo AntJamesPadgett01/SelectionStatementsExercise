000023a78e8dc3fd7affb197f302d5505f6d46dc
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-06 11:59:46 -0500

    Completed task

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favoriteNumber = 13;
+            Console.WriteLine("Guess the favorite number. Hint below 20");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favoriteNumber)
+            {
+                Console.WriteLine("Too LOW!!!");
+            }
+            else if (userGuess > favoriteNumber)
+            {
+                Console.WriteLine("Too HIGH!!");
+            }
+            else if (userGuess == favoriteNumber)
+            {
+                Console.WriteLine("You were correct, Great JOB!!!!");
+            }
+            else
+                Console.WriteLine("Nevermind");
+        }
+    }
+}
